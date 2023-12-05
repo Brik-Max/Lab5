798b578a9ae10ed1a9cab23ebd536cdfb1e23a75
Mr.BrickCH@outlook.com <Mr.BrickCH@outlook.com>
2023-12-05 14:33:18 +0700

    FileNotFound exeption & IOexeption

--- src/code/Main.java
+++ src/code/Main.java
@@ -1,5 +1,18 @@
+package code;
+import java.io.*;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try(FileReader reader = new FileReader("C:\\Users\\Maksim Brikun\\Desktop\\inf.txt")){
+            Scanner scan = new Scanner(reader); //связываем поток чтения со сканером
+            while(scan.hasNextLine()){ //проверям существует ли что-то в файле
+                System.out.println(scan.nextLine());//выводим в консоль
+            }
+        } catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
